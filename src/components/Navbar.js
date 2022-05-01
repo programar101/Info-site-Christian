78c9cb62a2260efb4af40ab495953599e028b637
Christian Carballo <carballo.chr@gmail.com>
2022-05-01 18:45:31 -0300

    css problem
    no aplica el css

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="https://spng.subpng.com/20180604/iqs/kisspng-react-javascript-angularjs-ionic-atom-5b154be64e7965.3537065815281223423214.jpg" alt="" />
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )}
