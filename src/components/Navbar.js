4f16566b7dfec4eb35ba2c07690d2182f470fcfd
Christian Carballo <carballo.chr@gmail.com>
2022-05-03 00:45:25 -0300

    css problemas

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,8 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav>
-            <img src="https://www.dlf.pt/dfpng/middlepng/521-5218160_react-image-create-react-app-logo-hd-png.png" alt="" />
-            <h3>ReactFacts</h3>
-            <h4>React Course - Project 1</h4>
+            <img className="nav--icon" src="https://www.dlf.pt/dfpng/middlepng/521-5218160_react-image-create-react-app-logo-hd-png.png" alt="" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
     )}
