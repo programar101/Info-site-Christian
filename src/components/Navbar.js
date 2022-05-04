834486902f64a40c698d86dcf6ed2facf6086ec8
Christian Carballo <carballo.chr@gmail.com>
2022-05-03 23:18:19 -0300

    CSS ok /Imagen ok
    No podía hacer andar el loguito de REACT pero ya pude :D

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
 import React from "react"
+import "../style.css"
+import logo from '../components/Images/logo.png'; 
+
+
 
 export default function Navbar() {
     return (
         <nav>
-            <img className="nav--icon" src="https://www.dlf.pt/dfpng/middlepng/521-5218160_react-image-create-react-app-logo-hd-png.png" alt="" />
+            <img className="nav--icon" src={logo}  alt="" />
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
